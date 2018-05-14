166d46afaf6c4316b2714e55a496fd30c567e9af
Dani Carter <carte470@d.umn.edu>
2018-05-13 19:10:42 -0500

    initial commit

---
+++ app/public/server.js
@@ -0,0 +1,18 @@
+// Dependencies
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+// var mixitup = require("mixitup");
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:"application/vnd.api+json"}));
+app.use(express.static('app/public'));
+require("./app/routes/htmlRoutes")(app);
+app.listen(PORT, function() {
+  console.log("App listening on PORT: " + PORT);
+});
