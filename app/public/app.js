334a99d7bac09978f6083c1dce588330922bd438
Dani Carter <carte470@d.umn.edu>
2018-05-17 19:01:45 -0500

    map on both pages

--- app/public/app.js
+++ app/public/app.js
@@ -1,7 +1,25 @@
+var park = {lat: 44.9398, lng: -93.2533};
 var map;
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-  		center: {lat: 44.9398, lng: -93.2533},
+  		center: park,
   		zoom: 15
 	});
+	var marker = new google.maps.Marker({
+		position:park,
+		map:map
+	});
 }
+
+// var park1 = {lat: 44.9398, lng: -93.2533};
+// var map1;
+// function initMap() {
+// 	map1 = new google.maps.Map(document.getElementById('map1'), {
+//   		center: park1,
+//   		zoom: 15
+// 	});
+// 	var marker1 = new google.maps.Marker({
+// 		position:park1,
+// 		map:map1
+// 	});
+// }
