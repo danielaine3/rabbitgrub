c0ee0e9164b1a010838a42992d955c481833aba2
Dani Carter <carte470@d.umn.edu>
2018-06-15 13:55:21 -0500

    map and caleander rendering

--- app/public/app.js
+++ app/public/app.js
@@ -1,3 +1,29 @@
+var caleandar;
+var events = [
+    {'Date': new Date(2018, 4, 19), 'Title': 'Powderhorn Shark Tank Final', 'Link': 'https://www.facebook.com/events/161369324497953/'},
+    {'Date': new Date(2018, 7, 4), 'Title': 'Powderhorn Art Fair Day 1', 'Link': 'https://www.facebook.com/powderhornartfair/'},
+    {'Date': new Date(2018, 7, 5), 'Title': 'Powderhorn Art Fair Day 2', 'Link': 'https://www.facebook.com/powderhornartfair/'}
+];
+
+var settings = {
+    Color: 'black',
+    LinkColor: 'teal',
+    NavShow: true,
+    NavVertical: false,
+    NavLocation: '',
+    DateTimeShow: true,
+    DateTimeFormat: 'mmm, yyyy',
+    DatetimeLocation: '',
+    EventClick: '',
+    EventTargetWholeDay: false,
+    DisabledDays: [],
+    ModelChange: '',
+};
+
+var element = document.getElementById('caleandar');
+
+caleandar(element, events, settings);
+
 window.lat = 44.9398;
 window.lng = -93.2533;
 
@@ -45,28 +71,5 @@ pubnub.addListener({message:redraw});
 
 setInterval(function() {
     pubnub.publish({channel:pnChannel, message:{lat:window.lat + 0.001, lng:window.lng + 0.01}});
-}, 500);
-
-var caleandar;
-var events = [
-	{'Date': new Date(2018, 4, 19), 'Title': 'Powderhorn Shark Tank Final', 'Link': 'https://www.facebook.com/events/161369324497953/'},
-];
-
-var settings = {
-    Color: 'black',
-    LinkColor: 'teal',
-    NavShow: true,
-    NavVertical: false,
-    NavLocation: '',
-    DateTimeShow: true,
-    DateTimeFormat: 'mmm, yyyy',
-    DatetimeLocation: '',
-    EventClick: '',
-    EventTargetWholeDay: false,
-    DisabledDays: [],
-    ModelChange: '',
-};
-
-var element = document.getElementById('caleandar');
-
-caleandar(element, events, settings);
+}, 
+500);
