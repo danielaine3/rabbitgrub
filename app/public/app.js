47d1eb3e450f5222ba8a153c59e4bd18c7d89bb1
Dani Carter <carte470@d.umn.edu>
2018-06-15 13:38:48 -0500

    map rendering on page

--- app/public/app.js
+++ app/public/app.js
@@ -1,23 +1,12 @@
 window.lat = 44.9398;
 window.lng = -93.2533;
-function circlePoint(time) {
-  var radius = 0.01;
-  var x = Math.cos(time) * radius;
-  var y = Math.sin(time) * radius;
-  return {lat:window.lat + y, lng:window.lng + x};
-};
 
 var map;
 var lineCoords = [];
 
-var pubnub = new PubNub({
-    publishKey:   'pub-c-47103c0c-b922-4ebd-ba7b-bd170efb6843',
-    subscribeKey: 'sub-c-a6c6a3b8-70c2-11e8-80d3-e2460f47e7ae'
-  });
-
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
-  		center: {lat:late, lng:lng}, 
+  		center: {lat:lat, lng:lng}, 
   		zoom: 15
 	});
 	var marker = new google.maps.Marker({
@@ -32,17 +21,17 @@ var redraw = function(payload) {
     lng = payload.message.lng;
     map.setCenter({lat:lat, lng:lng, alt:0});
     marker.setPosition({lat:lat, lng:lng, alt:0});
-  };
 
   lineCoords.push(new google.maps.LatLng(lat, lng));
 
-  var lineCoordinatesPath = new google.maps.Polyline({
+
+    var lineCoordinatesPath = new google.maps.Polyline({
     path: lineCoords,
     geodesic: true,
     strokeColor: '#2E10FF'
-  });
-  
-  lineCoordinatesPath.setMap(map);
+    });
+
+    lineCoordinatesPath.setMap(map);
 };
 
 var pnChannel = "map-channel";
@@ -50,13 +39,13 @@ var pubnub = new PubNub({
   publishKey:   'pub-c-47103c0c-b922-4ebd-ba7b-bd170efb6843',
   subscribeKey: 'sub-c-a6c6a3b8-70c2-11e8-80d3-e2460f47e7ae'
 });
+
 pubnub.subscribe({channels: [pnChannel]});
 pubnub.addListener({message:redraw});
 
 setInterval(function() {
-    pubnub.publish({channel:pnChannel, message:circlePoint(new Date().getTime()/1000)});
-  }, 500);
-};
+    pubnub.publish({channel:pnChannel, message:{lat:window.lat + 0.001, lng:window.lng + 0.01}});
+}, 500);
 
 var caleandar;
 var events = [
