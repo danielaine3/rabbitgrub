dd1e3326c32bc46196f0049cec2eb33ded202f70
Dani Carter <carte470@d.umn.edu>
2018-06-15 12:54:05 -0500

    pubnub initialized

--- app/public/app.js
+++ app/public/app.js
@@ -1,6 +1,11 @@
 var park = {lat: 44.9398, lng: -93.2533};
 var map;
 
+var pubnub = new PubNub({
+    publishKey:   'pub-c-47103c0c-b922-4ebd-ba7b-bd170efb6843',
+    subscribeKey: 'sub-c-a6c6a3b8-70c2-11e8-80d3-e2460f47e7ae'
+  });
+
 function initMap() {
 	map = new google.maps.Map(document.getElementById('map'), {
   		center: park,
